9ba4cce86b137db78e21b7ce1ba5399beea49504
matthew cannon <mcannon@ubuntu.(none)>
2013-04-11 17:00:57 +0100

    port from windows

--- rakefile.rb
+++ rakefile.rb
@@ -1,29 +1,29 @@
 task :default => [:recycle_app, :phantomjs, :cucumber, :stop_all]
 
-task :recycle_app => [:stop_all, :drop_db, :start_all]
+task :setup => [:clean, :install_node_modules]
 
-task :stop_all => [:stop_node, :stop_mongo]
+task :clean => [:clean_node_modules]
 
-task :start_all => [:start_mongo, :start_node]
-
-task :stop_mongo do
-	system 'taskkill /im mongod.exe /f /t'
+task :clean_node_modules do
+	system 'rm node_modules/ -r -f'
 end
 
-task :drop_db do
-	system 'rmdir db /q /s'
+task :install_node_modules do
+	system 'sudo npm install'
 end
 
-task :start_mongo do
-	system 'mkdir db && start mongod --dbpath .\db'
-end
+task :recycle_app => [:stop_all, :start_all]
+
+task :stop_all => [:stop_node]
+
+task :start_all => [:start_node]
 
 task :stop_node do
-	system 'taskkill /im node.exe /f /t'
+	system 'kill node'
 end
 
 task :start_node do
-	system 'start node express/app.js'
+	system 'node express/app.js'
 end
 
 task :cucumber do
@@ -31,9 +31,5 @@ task :cucumber do
 end
 
 task :phantomjs do
-	system 'tools/phantomjs/phantomjs.exe tools/phantomjs/examples/run-qunit.js test/tests.html'
-end
-
-task :setup_express do
-	system 'mkdir express && cd express && express -c stylus && npm install -d'
+	system './tools/phantomjs/bin/phantomjs tools/phantomjs/examples/run-qunit.js test/tests.html'
 end
