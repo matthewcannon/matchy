4179176cecfc40c21d12c76c56a0d30d66570333
Matthew <matthew@matthew-VirtualBox.(none)>
2014-03-26 11:55:31 +0000

    fixes the rakefile. adds the hexagonal diagram for dev-ops

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,6 @@
-task :default => [:recycle, :cucumber, :stop]
-
 task :setup => [:clean, :bundle, :install_node_modules]
+
+task :validate => [:recycle, :cucumber, :stop]
 
 task :clean => [:clean_bundle, :clean_node_modules]
 
@@ -47,15 +47,27 @@ task :remove_deleted_files_from_git do
     system 'git rm $(git ls-files --deleted)'
 end
 
-task :stage do
+task :stage => [:clean_stage, :package] do
+    system 'cp app.js ./pub/'
+    system 'cp app.html ./pub/'
     system 'cp Procfile ./pub/ -f'
     system 'cd ./pub/ && foreman start'
 end
 
-task :publish do
+task :clean_stage do
+    system 'rm ./pub/app.js -f'
+    system 'rm ./pub/app.html -f'
+    system 'rm ./pub/Procfile -f'
+end
+
+task :package do
+    system 'grunt minify'
+end
+
+task :ship do
     #system 'cd ./pub/'
     #system 'git add .'
-    #system 'git commit -m \'publish\'' 
+    #system 'git commit -m \'ship\'' 
     #system 'git push heroku master' 
     #system 'heroku ps:scale web=1'
     #system 'heroku ps' 
