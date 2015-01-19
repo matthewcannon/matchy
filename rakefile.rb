5186883639ebad65c658232300952a942bea71c7
mcannon <m.cannon.dev@gmail.com>
2015-01-19 14:37:10 +0000

    removes unused rake tasks. reconfigures express to serve from the current folder, which should be public

--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,4 @@
-task :setup => [:stop, :clean, :bundle, :install_node_modules]
+task :setup => [:stop_node, :clean, :bundle, :install_node_modules]
 
 task :clean => [:clean_bundle, :clean_node_modules, :clean_logs]
 
@@ -12,7 +12,6 @@ end
 
 task :clean_logs do
     system 'rm ./log/nodemon.log -f'
-    system 'rm ./log/mongo.log -f'
 end
 
 task :bundle do
@@ -23,34 +22,20 @@ task :install_node_modules do
 	system 'sudo npm install'
 end
 
-task :recycle => [:stop, :start]
-
-task :stop => [:stop_node, :stop_mongo]
-
-task :start => [:start_mongo, :start_node]
-
-task :stop_mongo do
-	system 'sudo killall mongod'
+task :start_node do
+	system 'nodemon ./public/server.js > ./log/nodemon.log &'
 end
 
 task :stop_node do
 	system 'killall node'
 end
 
-task :start_mongo do
-    system 'sudo mongod --fork -v --logpath ./log/mongo.log'
-end
-
-task :start_node do
-	system 'nodemon server.js > ./log/nodemon.log &'
-end
-
-task :check => [:recycle, :cucumber, :stop]
+task :check => [:start_node, :cucumber, :stop_node]
 
 task :cucumber do
 	system 'cd cucumber && bundle exec cucumber -o ../log/cucumber.log'
 end
 
-task :guard => [:recycle] do
+task :guard do
 	system 'cd cucumber && bundle exec guard -i'	
 end
--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var expressApp = express();
 
 expressApp.configure(function() {
-    expressApp.use(express.static(__dirname + '/public'));
+    // expressApp.use(express.static(__dirname + '/public'));
+    expressApp.use(express.static(__dirname));
 });
 
 expressApp.get('/', function(req, res) {
