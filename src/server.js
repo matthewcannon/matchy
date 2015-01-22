fefdf906a6cf778e2aa093b8f129be95082532e1
mcannon <m.cannon.dev@gmail.com>
2015-01-22 12:26:47 +0000

    configures express to serve all static content

--- src/server.js
+++ src/server.js
@@ -2,12 +2,7 @@ var express = require('express');
 var expressApp = express();
 
 expressApp.configure(function() {
-    // expressApp.use(express.static(__dirname + '/public'));
     expressApp.use(express.static(__dirname));
-});
-
-expressApp.get('/', function(req, res) {
-    res.sendfile('index.html');
 });
 
 expressApp.listen(process.env.PORT || 5000);
