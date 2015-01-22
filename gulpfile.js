d414ebcb50707ebd548dc678c91a74b81ae6597b
mcannon <m.cannon.dev@gmail.com>
2015-01-22 12:45:45 +0000

    gulp watches for javascript file changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 
 gulp.task('html', function() {
-    return gulp.src('./src/*.html')
-        .pipe(gulp.dest('./public/'));
+    return gulp.src('./src/*.html') .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('js', function() {
+    return gulp.src('./src/*.js') .pipe(gulp.dest('./public/'));
 });
 
 gulp.task('default', function() {
     gulp.watch('./src/*.html', ['html']);
+    gulp.watch('./src/*.js', ['js']);
 });
